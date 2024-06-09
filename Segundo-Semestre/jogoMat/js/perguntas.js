cee0291d9fdfa710ab59b4bf3e53a48e0cee127c
Keven Di Camargo Elpidio <102769882+Kevenshtk@users.noreply.github.com>
2024-06-09 16:26:50 -0300

    update perguntas.js
    corrigindo alternativas das perguntas finais

--- Segundo-Semestre/jogoMat/js/perguntas.js
+++ Segundo-Semestre/jogoMat/js/perguntas.js
@@ -438,7 +438,7 @@ const perguntasFinal = [
     {
         story: "Nas profundezas do Cóvil Sem Limites, atravessando por uma ponte de ossos sobre um lago de ácido, Dentro de uma cela escura e úmida, você encontra a princesa de Calculária, envolta em lágrimas silenciosas enquanto está encostada na parede. Assim que seus olhares se cruzam, a expressão da princesa se ilumina com um sorriso radiante de esperança. Ela te suplica em um sussurro desesperado para salvá-la.",
         question: "Qual é o f(x) da função 4X+2, quando X for 7?",
-        options: ["f'(x) = 1.", "f'(x) = 12.", "f'(x) = 25.", "f'(x) = 30."],
+        options: ["f(x) = 1.", "f(x) = 12.", "f(x) = 25.", "f(x) = 30."],
         correctAnswer: "f(x) = 30."
     },
 
@@ -452,14 +452,14 @@ const perguntasFinal = [
     {
         story: "Nas profundezas do Cóvil Sem Limites, atravessando por uma ponte de ossos sobre um lago de ácido, Dentro de uma cela escura e úmida, você encontra a princesa de Calculária, envolta em lágrimas silenciosas enquanto está encostada na parede. Assim que seus olhares se cruzam, a expressão da princesa se ilumina com um sorriso radiante de esperança. Ela te suplica em um sussurro desesperado para salvá-la.",
         question: "Qual é o f(x) da função 6X/2, quando X for 3?",
-        options: ["f'(x) = 9.", "f'(x) = 12.", "f'(x) = 25.", "f'(x) = 0."],
+        options: ["f(x) = 9.", "f(x) = 12.", "f(x) = 25.", "f(x) = 0."],
         correctAnswer: "f(x) = 9."
     },
 
     {
         story: "Nas profundezas do Cóvil Sem Limites, atravessando por uma ponte de ossos sobre um lago de ácido, Dentro de uma cela escura e úmida, você encontra a princesa de Calculária, envolta em lágrimas silenciosas enquanto está encostada na parede. Assim que seus olhares se cruzam, a expressão da princesa se ilumina com um sorriso radiante de esperança. Ela te suplica em um sussurro desesperado para salvá-la.",
         question: "Qual é o f(x) da função X²+3X-10, quando X for 5?",
-        options: ["f'(x) = 1.", "f'(x) = 30.", "f'(x) = 25.", "f'(x) = 35."],
+        options: ["f(x) = 1.", "f(x) = 30.", "f(x) = 25.", "f(x) = 35."],
         correctAnswer: "f(x) = 30."
     }
 ];
